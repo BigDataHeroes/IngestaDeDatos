dd7fae2904a54cb722db273ffc7ad1ff4e42aec9
ccalvomartinez <pececillu@gmail.com>
2018-10-09 10:49:39 +0200

    Añadidos requerimientos y main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 10:53:29 2018
+
+@author: Carolina
+"""
+import airbnb_crowler
+
+def get_airbnb_data(request):
+    airbnb_crowler.main(request)
+    
+
